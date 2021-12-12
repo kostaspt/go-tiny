2cc928b35e9e41ac2c52c043cb038cf12cce28bb
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2021-12-12 11:47:36 +0200

    change level to match zerolog's

--- pkg/logger/kratos.go
+++ pkg/logger/kratos.go
@@ -15,7 +15,7 @@ func NewForKratos() *Kratos {
 }
 
 func (Kratos) Log(level klog.Level, v ...interface{}) error {
-	logger := log.Level(zerolog.Level(level))
+	logger := log.Level(zerolog.Level(level + 1))
 	logger.Print(v...)
 	return nil
 }
